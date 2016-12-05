4173e1642bd80d3c4748b774c317d2cc018819af
wujs <wujs@act.buaa.edu.cn>
2016-12-05 10:29:51 +0800

    revise something

--- entityLinking/resize_input.py
+++ entityLinking/resize_input.py
@@ -1,7 +1,12 @@
 from __future__ import print_function
 import argparse
+import cPickle
 import os
-
+import sys
+sys.path.append('utils')
+sys.path.append('main1')
+sys.path.append('main2')
+from PhraseRecord import EntRecord
 
 def remove_crap(input_file):
     f = open(input_file)
@@ -17,7 +22,7 @@ def remove_crap(input_file):
     ff.close()
 
 
-def modify_data_size(output_file, trim):
+def modify_data_size(output_file, trim,sents):
     final_list = list()
     l = list()
     temp_len = 0
@@ -40,17 +45,30 @@ def modify_data_size(output_file, trim):
             l.append(line)
             temp_len += 1
     f = open(output_file, 'w')
+    #we need to filter the sentence contians non-ner! 
+    non_ents = 0
     for i in final_list:
-        f.writelines(i)
+        senti = u' '.join(i)
+        if ('LOC' in senti) or ('PER' in senti) or ('MISC' in senti) or ('ORG' in senti):
+            f.writelines(i)
+        else:
+            non_ents = non_ents + 1
+            print(i)
+            print('wrong....')
     f.close()
+    print('non entmentions sentences:%d' %(non_ents))
     print('%d sentences trimmed out of %d total sentences' % (count, len(final_list)))
     #os.system('rm temp.txt')
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', type=str, help='input file location', required=True)
+parser.add_argument('--entMentInput', type=str, help='entity mention files such as:train.p', required=True)
 parser.add_argument('--output', type=str, help='output file location', required=True)
 parser.add_argument('--trim', type=int, help='trimmed sentence length', required=True)
 args = parser.parse_args()
 remove_crap(args.input)
-modify_data_size(args.output, args.trim)
+data = cPickle.load(open(args.entMentInput,'r'))
+sents=data['sents']
+#print(sents)
+modify_data_size(args.output, args.trim,sents)
--- entityLinking/run.sh
+++ entityLinking/run.sh
@@ -3,7 +3,7 @@
 
 data="testa"
 dir_path="data/aida"
-
+dims="100"
 :<<!
 extract ent mention and its types
 !
@@ -23,19 +23,26 @@ generate entity mention candidates and related entities
 extract raw sentence to train word2vec
 !
 #python getSentenceEntMent.py  data/aida/process/ train.p train_sent.txt
-
-#python embeddings/wordvec_model.py --dir_path ${dir_path} --corpus ${dir_path}/process/train_sent.txt --dimension 100 --vocab_size 12000
+#python getSentenceEntMent.py  data/aida/process/ testa.p testa_sent.txt
+#python getSentenceEntMent.py  data/aida/process/ testb.p testb_sent.txt
+#cd data/aida/process/; cat testa_sent.txt testb_sent.txt train_sent.txt > total_sent.txt;cd ..;cd ..;cd ..;
+#python embeddings/wordvec_model.py --dir_path ${dir_path} --corpus ${dir_path}/process/total_sent.txt --dimension ${dims} #--vocab_size 23661
 
 
 :<<!
 resize your data into a max sequence length: such as 50(to speed up the trianing!)
 !
-
-#python resize_input.py --input data/aida/eng.train --output ${dir_path}/process/train.out --trim 50
-#python resize_input.py --input data/aida/eng.testa --output ${dir_path}/process/testa.out --trim 50
-#python resize_input.py --input data/aida/eng.testb --output ${dir_path}/process/testb.out --trim 50
+#python resize_input.py --input data/aida/eng.train --entMentInput data/aida/process/train.p --output ${dir_path}/process/train.out --trim 50
+#python resize_input.py --input data/aida/eng.testa --entMentInput data/aida/process/testa.p --output ${dir_path}/process/testa.out --trim 50
+#python resize_input.py --input data/aida/eng.testb --entMentInput data/aida/process/testb.p --output ${dir_path}/process/testb.out --trim 50
 
 :<<!
 get data embddings for bi-LSTM layers
 !
-python embeddings/get_conll_embeddings.py --dir_path ${dir_path} --data ${dir_path}/process/train.p --train ${dir_path}/process/train.out --test_a ${dir_path}/process/testa.out --test_b ${dir_path}/process/testb.out --use_model ${dir_path}/wordvec_model_100.p --model_dim 100
+python embeddings/get_conll_embeddings.py --dir_path ${dir_path} --data_train ${dir_path}/process/train.p --data_testa ${dir_path}/process/testa.p --data_testb ${dir_path}/process/testb.p --train ${dir_path}/process/train.out --test_a ${dir_path}/process/testa.out --test_b ${dir_path}/process/testb.out --use_model ${dir_path}/wordvec_model_${dims}.p --model_dim ${dims}
+
+
+:<<!
+generate cnn embedding datasets.
+!
+#python getctxCnnData.py ${dir_path} testa_candEnts.p 
