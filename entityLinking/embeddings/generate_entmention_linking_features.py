c28b0f5dfe4475a5cd1c90f778da08cedbba8650
wujs <wujs@act.buaa.edu.cn>
2016-12-27 20:51:11 +0800

    add entity candidates probability

--- entityLinking/embeddings/generate_entmention_linking_features.py
+++ entityLinking/embeddings/generate_entmention_linking_features.py
@@ -41,7 +41,7 @@ def processDescription(line):
         descript.append(token.text.lower())
         
   return descript
-
+        
 def get_all_candidate_mid_cocurrent(ent_Mentions,all_candidate_mids,fout):
   '''
   @2016/12/26 抽取候选实体间，是否在freebase中有共现
@@ -101,8 +101,8 @@ def get_candidate_rel_features(ent_Mentions,all_candidate_mids,fout,allcandents_
   print 'finish load all datas'
   ent_mention_relCoherent_feature=[]
   k=-1
-  #for i in tqdm(range(len(ent_Mentions))):
-  for i in tqdm(xrange(145,200)):
+  for i in tqdm(range(len(ent_Mentions))):
+  #for i in tqdm(xrange(145,200)):
     ents = ent_Mentions[i]  #ents 中的entities表示在同一个doc中的实体！
     tag = False
     doc_ents_cand_mid_dict=[]
@@ -173,6 +173,7 @@ def get_candidate_ent_features(ent_Mentions,all_candidate_mids,mid2description,d
   ent_mention_link_feature=[] #shape: [line,ent_mention_num, 30(candidates number) * 100(dimension)]
   ent_mention_tag = []  #shape:[line,ent_mention_num,30]
   ent_mention_type_feature=[]   #shape:[line,ent_mention_num,30* 113(one hot type dimension)]
+  ent_mention_cand_prob_feature=[]
   non_mid2figer = 0
   k  = -1
   for i in tqdm(range(len(ent_Mentions))):
@@ -181,6 +182,7 @@ def get_candidate_ent_features(ent_Mentions,all_candidate_mids,mid2description,d
     temps =[]
     temps_type=[]
     temps_tag = []
+    temps_cand_prob=[]
     temps_ent_index=[]
     #print i,'\tentM:',len(ents)
     for j in range(len(ents)):
@@ -194,67 +196,73 @@ def get_candidate_ent_features(ent_Mentions,all_candidate_mids,mid2description,d
         k += 1
         tag = entstr_lower2mid[enti_name]
       
-      if tag !=False:
-        tag_t = 0
-        ent_mention_tag_temp = np.zeros((30,))
-        tdescip = []
-        tcanditetype=[]
-        cand_mid_dict = all_candidate_mids[k]
-    
-        for mid in cand_mid_dict:  #仅有description,实体共现！
-          if mid not in mid2description and mid == tag:   #用来去抓取需要linking的实体啦！,filter to ensure all the result candidates has the description!
-            print 'mide not in mid2description', mid
-          if mid == tag:
-            ent_mention_tag_temp[tag_t] = 1
-          tag_t += 1
-          if mid in mid2description:
-            twordv = np.zeros((100,)) 
-            ttypev = np.zeros((113,))
-            if mid in mid2figer:
-              for midtype in mid2figer[mid]:
-                ttypev[midtype]=1
+      
+      tag_t = 0
+      ent_mention_tag_temp = np.zeros((30,))
+      ent_mention_prob_temp = np.zeros((30,))
+      tdescip = []
+      tcanditetype=[]
+      cand_mid_dict = all_candidate_mids[k]
   
-            line = mid2description[mid]
-            descript = processDescription(line)
-            descrip_lent.append(len(descript))
-            for i in range(min(15,len(descript))):
-              word = descript[i]
-              if word in descript_Words:
-                twordv += descript_Words[word]
-            '''
-            @add candidate entity type features!
-            '''
-            if len(tcanditetype)==0:
-              tcanditetype = ttypev
-            else:
-              tcanditetype = np.concatenate((tcanditetype,ttypev))
-              
-            if len(tdescip)==0:
-              tdescip = twordv
-            else:
-              tdescip = np.concatenate((tdescip,twordv))
-        temps_type.append(tcanditetype)
-        temps.append(tdescip)
-        temps_tag.append(ent_mention_tag_temp)
-        temps_ent_index.append((enti.startIndex,enti.endIndex))  #通过这个flag去抽取lstm最后一层的特征啦！
+      for mid in cand_mid_dict:  #仅有description,实体共现！
+        ent_mention_prob_temp[tag_t] = cand_mid_dict[mid]  #产生information retrieval每个候选实体的概率p(e|m)
+        
+        if mid not in mid2description and mid == tag:   #用来去抓取需要linking的实体啦！,filter to ensure all the result candidates has the description!
+          print 'mide not in mid2description', mid
+        if mid == tag:
+          ent_mention_tag_temp[tag_t] = 1
+        tag_t += 1
+        
+        twordv = np.zeros((100,)) 
+        ttypev = np.zeros((113,))
+        if mid in mid2figer:         #也有很多实体并没有figer的type所以这个特征也为0
+          for midtype in mid2figer[mid]:
+            ttypev[midtype]=1
+           
+        if mid in mid2description:  #不需要这个条件的哈，因为很多实体都缺少这个条件，对于那些不popular的候选实体啦！
+          line = mid2description[mid]
+          descript = processDescription(line)
+          descrip_lent.append(len(descript))
+          for i in range(min(15,len(descript))):
+            word = descript[i]
+            if word in descript_Words:
+              twordv += descript_Words[word]
+        '''
+        @add candidate entity type features!
+        '''
+        if len(tcanditetype)==0:
+          tcanditetype = ttypev
+        else:
+          tcanditetype = np.concatenate((tcanditetype,ttypev))
+          
+        if len(tdescip)==0:
+          tdescip = twordv
+        else:
+          tdescip = np.concatenate((tdescip,twordv))
+      temps_type.append(tcanditetype)
+      temps.append(tdescip)
+      temps_tag.append(ent_mention_tag_temp)
+      temps_cand_prob.append(ent_mention_prob_temp)
+      temps_ent_index.append((enti.startIndex,enti.endIndex))  #通过这个flag去抽取lstm最后一层的特征啦！
     ent_mention_type_feature.append(temps_type)
     ent_mention_link_feature.append(temps)
     ent_mention_tag.append(temps_tag)
+    ent_mention_cand_prob_feature.append(temps_cand_prob)
     ent_mention_index.append(temps_ent_index)   
   print len(ent_mention_link_feature),len(ent_mention_link_feature[3]),len(ent_mention_link_feature[3][0])
   print len(ent_mention_tag),len(ent_mention_tag[3]),len(ent_mention_tag[3][0])
   print len(ent_mention_index),len(ent_mention_index[3]),len(ent_mention_index[3][0])
   print  max(descrip_lent),min(descrip_lent),sum(descrip_lent) / float(len(descrip_lent))
   param_dict = {'ent_mention_index':ent_mention_index,'ent_mention_link_feature':ent_mention_link_feature,'ent_mention_tag':ent_mention_tag}
-  #cPickle.dump(param_dict,open(f_output,'wb'))
+  cPickle.dump(param_dict,open(f_output,'wb'))
   print len(ent_mention_type_feature),len(ent_mention_type_feature[3]),len(ent_mention_type_feature[3][0])
   cPickle.dump(ent_mention_type_feature,open(f_output1,'wb'))
-  
+  cPickle.dump(ent_mention_cand_prob_feature,open(f_output2,'wb'))
   print non_mid2figer
   
 if __name__=='__main__':
-  if len(sys.argv) !=8:
-    print 'usage: python pyfile dir_path testa_entms.p100(test) test_a_embed.p testa_ent_cand_mid.p testa_ent_linking.p testa_ent_linking_type.p testa_ent_relcoherent.p'
+  if len(sys.argv) !=9:
+    print 'usage: python embeddings/generate_entmention_linking_features.py ${dir_path} testa_entms.p100 test_a_embed.p100 testa_ent_cand_mid.p testa_ent_linking.p testa_ent_linking_type.p testa_ent_linking_candprob.p testa_ent_relcoherent.p'
     exit(1)
   dir_path = sys.argv[1]
   f_input_ent_ments = dir_path + '/features/' + sys.argv[2]
@@ -262,8 +270,9 @@ if __name__=='__main__':
   f_input_ent_cand_mid = dir_path  +'/features/'+ sys.argv[4]
   f_output = dir_path  +'/features/'+ sys.argv[5]
   f_output1 = dir_path  +'/features/'+ sys.argv[6]
-  f_outputtemp = dir_path  +'/features/'+ sys.argv[7]+'temp'
   f_output2 = dir_path  +'/features/'+ sys.argv[7]
+  f_outputtemp = dir_path  +'/features/'+ sys.argv[8]+'temp'
+  f_output3 = dir_path  +'/features/'+ sys.argv[8]
   '''
   @time: 2016/12/23
   @function: load re-rank feature：entity type
@@ -279,10 +288,13 @@ if __name__=='__main__':
   dataEnts = cPickle.load(open(f_input_ent_ments,'rb'));ent_Mentions = dataEnts['ent_Mentions']
   all_candidate_mids = cPickle.load(open(f_input_ent_cand_mid,'rb'))
   print 'load all data cost time:',time.time()-stime
-  get_all_candidate_mid_cocurrent(ent_Mentions,all_candidate_mids,f_outputtemp)
+  
+  '''
+  get_all_candidate_mid_cocurrent(ent_Mentions,all_candidate_mids,f_outputtemp)   #之前已经从freebase里面抽取了的！
   allcandents_coents = cPickle.load(open(f_outputtemp,'rb'))
-  get_candidate_rel_features(ent_Mentions,all_candidate_mids,f_output2,allcandents_coents)
-
+  get_candidate_rel_features(ent_Mentions,all_candidate_mids,f_output3,allcandents_coents)
+  '''
+  
   '''
   totalEntNum = 0
   for i in tqdm(range(len(ent_Mentions))):
@@ -299,9 +311,10 @@ if __name__=='__main__':
   
   #sent_embed = cPickle.load(open(f_input_ent_embed,'rb'))
   #assert len(ent_Mentions) == len(sent_embed)
-  
+  '''
+  stime = time.time()
   descript_Words = cPickle.load(open('/home/wjs/demo/entityType/informationExtract/data/wordvec_model_100.p', 'rb'))
-  print 'load all related data cost time: ', time.time()-stime
+  print 'load wordvec_model_100 cost time: ', time.time()-stime
   
   stime = time.time()
   mid2description={}  #nearly 2.3G
@@ -311,8 +324,8 @@ if __name__=='__main__':
       if len(items) >=2:
         mid2description[items[0]] =items[1]
   print 'load mid2descriptioon cost time: ', time.time()-stime
-  #get_candidate_ent_features(ent_Mentions,all_candidate_mids,mid2description,descript_Words.wvec_model,mid2figer,f_output,f_output1)
-  '''
+  get_candidate_ent_features(ent_Mentions,all_candidate_mids,mid2description,descript_Words.wvec_model,mid2figer,f_output,f_output1)
+  
   '''
   param_dict = cPickle.load(open(f_output,'rb'))
   ent_mention_link_feature = param_dict['ent_mention_link_feature']
