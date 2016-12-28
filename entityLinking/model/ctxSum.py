8ddd02a65d14f724d8aa6bf52828e7f7af60681b
wujs <wujs@act.buaa.edu.cn>
2016-12-28 10:34:17 +0800

    add features for entity linking

--- entityLinking/model/ctxSum.py
+++ entityLinking/model/ctxSum.py
@@ -15,52 +15,52 @@ class ctxSum(object):
   '''
   def __init__(self,args):
     with tf.variable_scope("ctxSum") as scope:
-      #entity linking 需要修正的是bilinear weights
+      #entity linking 需要修正的是bilinear weights           
       self.bilinear_w_descrip = tf.get_variable(
                     "bilinear_w_description",
                     shape=[args.rawword_dim,2*args.rnn_size],
                     initializer=tf.contrib.layers.xavier_initializer())
-      print self.bilinear_w_descrip 
-      
+      print self.bilinear_w_descrip
+                  
       self.bilinear_w_type = tf.get_variable(
                     "bilinear_w_type",
                     shape=[args.figer_type_num,2*args.rnn_size],
                     initializer=tf.contrib.layers.xavier_initializer())
       print self.bilinear_w_type
-    
     #for every entity mention, there are 30 candidates entities
     self.ent_mention_linking_tag = tf.placeholder(tf.float32,[None,args.candidate_ent_num])
     self.candidate_ent_linking_feature= tf.placeholder(tf.float32,[None,args.candidate_ent_num,args.rawword_dim])
     self.candidate_ent_type_feature = tf.placeholder(tf.float32,[None,args.candidate_ent_num,args.figer_type_num])
     self.ent_mention_lstm_feature = tf.placeholder(tf.float32,[None,2*args.rnn_size,1])
+    self.candidate_ent_coherent_feature = tf.placeholder(tf.float32,[None,args.candidate_ent_num])
     
     
     self.candidate_ent_linking_feature = tf.nn.l2_normalize(self.candidate_ent_linking_feature,1)
     self.ent_mention_lstm_feature = tf.nn.l2_normalize(self.ent_mention_lstm_feature,1)
-    
+    self.candidate_ent_type_feature = tf.nn.l2_normalize(self.candidate_ent_type_feature,1)
+    self.candidate_ent_coherent_feature = tf.nn.l2_normalize(self.candidate_ent_coherent_feature,1)
     '''
     @2016/12/22 不知道为啥einsum对不定长的数据就失效了呢！
-    '''
     #temp =  tf.einsum('ijk,kl->ijl',self.candidate_ent_linking_feature,self.bilinear_w)
-    
     #tf.scan 或者 tf.einsum 都可以解决这个问题！tensorflow 丰富的函数库，可以减少很多工作呢！
-    temp_descrip = tf.scan(lambda a,x: tf.matmul(x,self.bilinear_w_descrip),self.candidate_ent_linking_feature,initializer=tf.Variable(tf.random_normal((args.candidate_ent_num,2*args.rnn_size))))
+    '''
+    temp_descrip = tf.scan(lambda a,x: tf.nn.relu(tf.matmul(x,self.bilinear_w_descrip)),self.candidate_ent_linking_feature,initializer=tf.Variable(tf.random_normal((args.candidate_ent_num,2*args.rnn_size))))
     print temp_descrip
-    
+  
     temp_type = tf.scan(lambda a,x: tf.matmul(x,self.bilinear_w_type),self.candidate_ent_type_feature,initializer=tf.Variable(tf.random_normal((args.candidate_ent_num,2*args.rnn_size))))
     print temp_type
     
-    self.prediction_descrip =  tf.batch_matmul(temp_descrip, self.ent_mention_lstm_feature)
-    self.prediction_type =  tf.batch_matmul(temp_type, self.ent_mention_lstm_feature)
+    self.prediction_descrip = tf.batch_matmul(temp_descrip,self.ent_mention_lstm_feature)
+    self.prediction_type = tf.batch_matmul(temp_type,self.ent_mention_lstm_feature)
     
-    self.prediction = tf.nn.softmax(tf.add(self.prediction_descrip[:,:,0],self.prediction_type[:,:,0]))
+    self.prediction = tf.nn.softmax(tf.add(self.candidate_ent_coherent_feature,tf.add(self.prediction_descrip[:,:,0],self.prediction_type[:,:,0])))
     
     print 'ctx prediction:',self.prediction
     print 'ctxSum ent_mention_linking_tag', self.ent_mention_linking_tag
-    self.linking_loss = tf.contrib.losses.softmax_cross_entropy(logits=self.prediction, onehot_labels=self.ent_mention_linking_tag)
+    
+    self.linking_loss  = tf.reduce_mean(-tf.reduce_sum(self.ent_mention_linking_tag * tf.log(self.prediction+1e-9), reduction_indices=[1]))
     
     print 'linking_loss:', self.linking_loss
     
-    correct_predict = tf.equal(tf.argmax(self.ent_mention_linking_tag,1),tf.argmax(self.prediction,1))
+    correct_predict = tf.equal(tf.argmax(self.prediction,1),tf.argmax(self.ent_mention_linking_tag,1))
     self.accuracy = tf.reduce_mean(tf.cast(correct_predict,'float'))
-    
--- entityLinking/trainCtxLSTM.py
+++ entityLinking/trainCtxLSTM.py
@@ -16,6 +16,7 @@ from utils import inputUtils
 import pprint
 import random
 import time
+from sklearn.metrics import f1_score
 from tqdm import tqdm
 pp = pprint.PrettyPrinter()
 
@@ -66,10 +67,12 @@ def f1(args, prediction, target, length):
   return fscore
 
 
-def getLinkingFeature(lstm_output,ent_mention_index,ent_mention_tag,ent_mention_link_feature,ent_linking_type,ptr):
+def getLinkingFeature(lstm_output,ent_mention_index,ent_mention_tag,ent_relcoherent,ent_mention_link_feature,ent_linking_type,ptr):
+#def getLinkingFeature(ent_mention_index,ent_mention_tag,ent_relcoherent,ent_mention_link_feature,ent_linking_type,ptr):
   ent_mention_linking_tag_list = []
   candidate_ent_linking_feature=[]
   candidate_ent_type_feature=[]
+  candidate_ent_relcoherent_feature=[]
   ent_mention_lstm_feature = []
   allLenght = len(ent_mention_index)
   
@@ -84,6 +87,8 @@ def getLinkingFeature(lstm_output,ent_mention_index,ent_mention_tag,ent_mention_
         
         ent_mention_linking_tag = np.asarray(ent_mention_tag[ids][tagid],dtype=np.float32)
         ent_mention_linking_tag_list.append(ent_mention_linking_tag)
+        candidate_ent_relcoherent_item = np.asarray(ent_relcoherent[ids][tagid],dtype=np.float32)
+        candidate_ent_relcoherent_feature.append(candidate_ent_relcoherent_item)
         
         candidate_num = len(ent_mention_link_feature[ids][tagid])/100 #这种求解的方法貌似有问题呢！
         ent_linking_candidates =  np.concatenate((np.asarray(np.reshape(ent_mention_link_feature[ids][tagid],(candidate_num,100)),dtype=np.float32), 
@@ -96,12 +101,23 @@ def getLinkingFeature(lstm_output,ent_mention_index,ent_mention_tag,ent_mention_
         ent_mention_lstm_feature.append(np.sum(lstm_output[ids-ptr][ent_item[0]:ent_item[1]],axis=0))
       tagid += 1
   ent_mention_linking_tag_list = np.asarray(ent_mention_linking_tag_list)
+  candidate_ent_relcoherent_feature = np.asarray(candidate_ent_relcoherent_feature)
   candidate_ent_linking_feature = np.asarray(candidate_ent_linking_feature)
   candidate_ent_type_feature = np.asarray(candidate_ent_type_feature)
   ent_mention_lstm_feature = np.expand_dims(np.asarray(ent_mention_lstm_feature),2)
   
-  return ent_mention_linking_tag_list,candidate_ent_linking_feature,candidate_ent_type_feature,ent_mention_lstm_feature
-
+  return ent_mention_linking_tag_list,candidate_ent_linking_feature,candidate_ent_type_feature,ent_mention_lstm_feature,candidate_ent_relcoherent_feature
+  
+def shuffle_in_unison(a, b):
+  assert len(a) == len(b)
+  shuffled_a = np.empty(a.shape, dtype=a.dtype)
+  shuffled_b = np.empty(b.shape, dtype=b.dtype)
+  permutation = np.random.permutation(len(a))
+  for old_index, new_index in enumerate(permutation):
+    shuffled_a[new_index] = a[old_index]
+    shuffled_b[new_index] = b[old_index]
+  return shuffled_a, shuffled_b
+    
 def main(_):
   pp.pprint(flags.FLAGS.__flags)
   if not os.path.exists(args.restore):
@@ -114,32 +130,31 @@ def main(_):
   loss_ent = model.loss
   
   modelLinking = ctxSum(args)
-  loss_linking = modelLinking.linking_loss + 0.01*tf.nn.l2_loss(modelLinking.bilinear_w_descrip)+0.01*tf.nn.l2_loss(modelLinking.bilinear_w_type)
+  loss_linking = modelLinking.linking_loss #+ 0.01*tf.nn.l2_loss(modelLinking.bilinear_w_descrip)+0.01*tf.nn.l2_loss(modelLinking.bilinear_w_type)
   
   '''
   @function: load the train and test datasets
   @entlinking context: 'ent_mention_index':ent_mention_index,'ent_mention_link_feature':ent_mention_link_feature,'ent_mention_tag':ent_mention_tag
   '''
   print 'start to load data...'
+  
   start = time.time()
+  testaUtils = inputUtils(args.rawword_dim,"testa")
+  testa_input = testaUtils.emb; testa_out = testaUtils.tag; testa_entliking= testaUtils.ent_linking; 
+  testa_ent_mention_index = testa_entliking['ent_mention_index']; testa_ent_mention_link_feature=testa_entliking['ent_mention_link_feature'];
+  testa_ent_mention_tag = testa_entliking['ent_mention_tag']; testa_ent_relcoherent = testaUtils.ent_relcoherent
+  testa_ent_linking_type = testaUtils.ent_linking_type
   
   trainUtils = inputUtils(args.rawword_dim,"train")
   train_input = trainUtils.emb; train_out = trainUtils.tag; train_entliking= trainUtils.ent_linking; 
   train_ent_mention_index = train_entliking['ent_mention_index']; train_ent_mention_link_feature=train_entliking['ent_mention_link_feature'];
-  train_ent_mention_tag = train_entliking['ent_mention_tag']
+  train_ent_mention_tag = train_entliking['ent_mention_tag']; train_ent_relcoherent = trainUtils.ent_relcoherent
   train_ent_linking_type = trainUtils.ent_linking_type
-  
-  
-  testaUtils = inputUtils(args.rawword_dim,"testa")
-  testa_input = testaUtils.emb; testa_out = testaUtils.tag; testa_entliking= testaUtils.ent_linking; 
-  testa_ent_mention_index = testa_entliking['ent_mention_index']; testa_ent_mention_link_feature=testa_entliking['ent_mention_link_feature'];
-  testa_ent_mention_tag = testa_entliking['ent_mention_tag']
-  testa_ent_linking_type = testaUtils.ent_linking_type
   
   testbUtils = inputUtils(args.rawword_dim,"testb")
   testb_input = testbUtils.emb; testb_out = testbUtils.tag; testb_entliking= testbUtils.ent_linking
   testb_ent_mention_index = testb_entliking['ent_mention_index']; testb_ent_mention_link_feature=testb_entliking['ent_mention_link_feature'];
-  testb_ent_mention_tag = testb_entliking['ent_mention_tag']
+  testb_ent_mention_tag = testb_entliking['ent_mention_tag']; testb_ent_relcoherent = testbUtils.ent_relcoherent
   testb_ent_linking_type = testbUtils.ent_linking_type
   print 'cost:', time.time()-start,' to load data'
   
@@ -158,8 +173,9 @@ def main(_):
   tvars_linking = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope='ctxSum')
   print 'tvars_linking:',tvars_linking
   
-  optimizer2 = tf.train.AdamOptimizer(args.learning_rate)
-  gradsLinking,_ = tf.clip_by_global_norm(tf.gradients(loss_linking,tvars_linking),10)
+  #optimizer2 = tf.train.GradientDescentOptimizer(0.2)
+  optimizer2 = tf.train.AdamOptimizer(0.05)
+  gradsLinking,_ = tf.clip_by_global_norm(tf.gradients(loss_linking,tvars_linking),5)
   train_op_linking = optimizer2.apply_gradients(zip(gradsLinking,tvars_linking))
   
   print 'start to initialize parameters'
@@ -179,24 +195,29 @@ def main(_):
   for e in range(args.epoch):
     id_epoch = 0
     print 'Epoch: %d------------' %(e)
-    average_linking_accuracy = 0
+    average_linking_accuracy_train = 0;average_loss_train = 0
     for ptr in xrange(0,len(train_input),args.batch_size):
       id_epoch = id_epoch + 1
-      _,lstm_output = sess.run([train_op,model.output], {model.input_data:train_input[ptr:min(ptr+args.batch_size,len(train_input))],
+      _,lstm_output = sess.run([train_op,model.output], 
+                        {model.input_data:train_input[ptr:min(ptr+args.batch_size,len(train_input))],
                           model.output_data:train_out[ptr:min(ptr+args.batch_size,len(train_input))],
                           model.keep_prob:0.5})
        
-      ent_mention_linking_tag_list,candidate_ent_linking_feature,candidate_ent_type_feature,ent_mention_lstm_feature = \
-                                                  getLinkingFeature(lstm_output,train_ent_mention_index,train_ent_mention_tag,train_ent_mention_link_feature,train_ent_linking_type,ptr)
+      ent_mention_linking_tag_list,candidate_ent_linking_feature,candidate_ent_type_feature,ent_mention_lstm_feature,ent_relcoherent_feature = \
+                                                  getLinkingFeature(lstm_output,train_ent_mention_index,train_ent_mention_tag,\
+                                                  train_ent_relcoherent,train_ent_mention_link_feature,train_ent_linking_type,ptr)
        
-      _, loss2,accuracy,l2_loss,linkLoss = sess.run([train_op_linking,loss_linking,modelLinking.accuracy,tf.nn.l2_loss(modelLinking.bilinear_w_type),modelLinking.linking_loss],
+      _, loss2,accuracy,pred = sess.run([train_op_linking,loss_linking,modelLinking.accuracy,modelLinking.prediction],
                                  {modelLinking.ent_mention_linking_tag:ent_mention_linking_tag_list,
+                                  modelLinking.candidate_ent_coherent_feature:ent_relcoherent_feature,
                                   modelLinking.candidate_ent_linking_feature:candidate_ent_linking_feature,
                                   modelLinking.candidate_ent_type_feature:candidate_ent_type_feature,
                                   modelLinking.ent_mention_lstm_feature:ent_mention_lstm_feature
                                  })
-      print 'total loss:',loss2, 'l2_loss:',l2_loss, 'linkingloss:',linkLoss
-      average_linking_accuracy += accuracy 
+      f1_micro,f1_macro =f1_score(np.argmax(ent_mention_linking_tag_list,1),np.argmax(pred,1),average='micro'),f1_score(np.argmax(ent_mention_linking_tag_list,1),np.argmax(pred,1),average='macro')
+      #print 'total loss:',loss2,' accuracy:',accuracy,' f1_micro:',f1_micro,' f1_macro:',f1_macro
+      average_linking_accuracy_train += accuracy 
+      average_loss_train += loss2
       if id_epoch%20==0:
         loss1,pred,length = sess.run([loss_ent,model.prediction,model.length],
                           {model.input_data:train_input[ptr:ptr+args.batch_size],
@@ -205,24 +226,51 @@ def main(_):
         fscore = f1(args, pred, train_out[ptr:ptr+args.batch_size], length)
         print("train: loss:%.4f NER:%.2f LOC:%.2f MISC:%.2f ORG:%.2f PER:%.2f" %(loss1,100*fscore[5],100*fscore[1],100*fscore[3],100*fscore[2],100*fscore[0]))
         
-                    
-        loss1,pred,length = sess.run([loss_ent,model.prediction,model.length],
+        '''
+        @function: testa(dev) 
+        '''
+        loss1,pred,length,lstm_output = sess.run([loss_ent,model.prediction,model.length,model.output],
                              {model.input_data:testa_input,
                               model.output_data:testa_out,
                               model.keep_prob:1})
         fscore = f1(args, pred, testa_out, length)
         print "-----------------"
         print("testa: loss:%.4f NER:%.2f LOC:%.2f MISC:%.2f ORG:%.2f PER:%.2f" %(loss1,100*fscore[5],100*fscore[1],100*fscore[3],100*fscore[2],100*fscore[0]))
+        ent_mention_linking_tag_list,candidate_ent_linking_feature,candidate_ent_type_feature,ent_mention_lstm_feature,ent_relcoherent_feature = \
+                                                  getLinkingFeature(lstm_output,teata_ent_mention_index,testa_ent_mention_tag,\
+                                                  testa_ent_relcoherent,testa_ent_mention_link_feature,testa_ent_linking_type,ptr)
+        _, loss2,accuracy,pred = sess.run([train_op_linking,loss_linking,modelLinking.accuracy,modelLinking.prediction],
+                                 {modelLinking.ent_mention_linking_tag:ent_mention_linking_tag_list,
+                                  modelLinking.candidate_ent_coherent_feature:ent_relcoherent_feature,
+                                  modelLinking.candidate_ent_linking_feature:candidate_ent_linking_feature,
+                                  modelLinking.candidate_ent_type_feature:candidate_ent_type_feature,
+                                  modelLinking.ent_mention_lstm_feature:ent_mention_lstm_feature
+                                 })
+        f1_micro_testa,f1_macro_testb =f1_score(np.argmax(ent_mention_linking_tag_list,1),np.argmax(pred,1),average='micro'),f1_score(np.argmax(ent_mention_linking_tag_list,1),np.argmax(pred,1),average='macro')
+        print 'testa total loss:',loss2,' accuracy:',accuracy,' f1_micro:',f1_micro,' f1_macro:',f1_macro
+        
         m = fscore[args.class_size]
         if m > maximum:
           maximum = m
-          loss1,pred,length = sess.run([loss_ent,model.prediction,model.length],
+          loss1,pred,length,lstm_output = sess.run([loss_ent,model.prediction,model.length,model.output],
                              {model.input_data:testb_input,
                               model.output_data:testb_out,
                               model.keep_prob:1})
           fscore = f1(args, pred, testb_out, length)
           print("testb: loss:%.4f NER:%.2f LOC:%.2f MISC:%.2f ORG:%.2f PER:%.2f" %(loss1,100*fscore[5],100*fscore[1],100*fscore[3],100*fscore[2],100*fscore[0]))
+          ent_mention_linking_tag_list,candidate_ent_linking_feature,candidate_ent_type_feature,ent_mention_lstm_feature,ent_relcoherent_feature = \
+                                                  getLinkingFeature(lstm_output,teatb_ent_mention_index,testb_ent_mention_tag,\
+                                                  testb_ent_relcoherent,testb_ent_mention_link_feature,testb_ent_linking_type,ptr)
+          _, loss2,accuracy,pred = sess.run([train_op_linking,loss_linking,modelLinking.accuracy,modelLinking.prediction],
+                                 {modelLinking.ent_mention_linking_tag:ent_mention_linking_tag_list,
+                                  modelLinking.candidate_ent_coherent_feature:ent_relcoherent_feature,
+                                  modelLinking.candidate_ent_linking_feature:candidate_ent_linking_feature,
+                                  modelLinking.candidate_ent_type_feature:candidate_ent_type_feature,
+                                  modelLinking.ent_mention_lstm_feature:ent_mention_lstm_feature
+                                 })
+          f1_micro_testa,f1_macro_testb =f1_score(np.argmax(ent_mention_linking_tag_list,1),np.argmax(pred,1),average='micro'),f1_score(np.argmax(ent_mention_linking_tag_list,1),np.argmax(pred,1),average='macro')
+          print 'testb total loss:',loss2,' accuracy:',accuracy,' f1_micro:',f1_micro,' f1_macro:',f1_macro
         print "-----------------" 
-    print 'average linking accuracy:',average_linking_accuracy/id_epoch
+    print 'average linking accuracy:',average_linking_accuracy/id_epoch, average_loss/id_epoch
 if __name__ == '__main__':
   tf.app.run()      
