680be28573510d27580b7fe929a881bd27ca7e94
wujs <wujs@act.buaa.edu.cn>
2017-04-28 13:50:25 +0800

    fully connection layer

--- NEMTyping/model/seqLSTM.py
+++ NEMTyping/model/seqLSTM.py
@@ -22,21 +22,21 @@ class seqLSTM(Model):
     self.args = args
     self.batch_size=args.batch_size
     
-    with tf.device('/gpu:0'):
+    with tf.device('/gpu:1'):
       self.input_data = tf.placeholder(tf.float32,[self.batch_size,self.args.sentence_length,self.args.word_dim])
     #need add
     #self.output_data = tf.placeholder(tf.float32,[None,self.args.sentence_length,self.args.class_size])
     #self.output_data = tf.sparse_placeholder(tf.float32, [None,self.args.sentence_length,self.args.class_size], name='outputdata')
     #self.output_data = tf.placeholder(tf.int32,[None,self.args.sentence_length])
-    with tf.device('/gpu:1'):
-      self.output_data = tf.sparse_placeholder(tf.float32, name='outputdata')  #[None, 114]
-      self.keep_prob = tf.placeholder(tf.float32,name='keep_prob_NER')
-      self.num_examples = tf.placeholder(tf.int32,name='num_examples')
+    
+    self.output_data = tf.sparse_placeholder(tf.float32, name='outputdata')  #[None, 114]
+    self.keep_prob = tf.placeholder(tf.float32,name='keep_prob_NER')
+    self.num_examples = tf.placeholder(tf.int32,name='num_examples')
 
-      self.entMentIndex = tf.placeholder(tf.int32,[None,None],name='ent_mention_index')
+    self.entMentIndex = tf.placeholder(tf.int32,[None,None],name='ent_mention_index')
       
     with tf.variable_scope("seqLSTM_variables") as scope:
-      with tf.device('/gpu:0'):
+      with tf.device('/gpu:1'):
         self.crf_weights = tf.get_variable("crf_weights",
                                      shape=[2*self.args.rnn_size,self.args.class_size],
                                      initializer=tf.contrib.layers.xavier_initializer())
@@ -57,7 +57,7 @@ class seqLSTM(Model):
         output,_,_ = tf.nn.bidirectional_rnn(fw_cell,bw_cell,
                                             tf.unpack(tf.transpose(self.input_data,perm=[1,0,2])),
                                             dtype=tf.float32,sequence_length=self.length
-                                            )
+                                            ,scope=scope)
       with tf.device('/cpu:0'):  #fix run on cpus
         if self.args.dropout:
           output =  tf.nn.dropout(output,self.keep_prob)
@@ -80,22 +80,26 @@ class seqLSTM(Model):
         '''
         @time: 2017/4/26 very efficient methods to get entity mentions embeddings!
         '''
-        output_f = tf.reduce_sum(tf.nn.embedding_lookup(self.output,self.entMentIndex),1)
+        output_f = tf.reduce_mean(tf.nn.embedding_lookup(self.output,self.entMentIndex),1)  #reduce_sum vs. reduce_mean
         print 'output_f:',output_f
-        W = tf.get_variable(
-                  "W1",
-                  shape=[2*self.args.rnn_size, self.args.rnn_size],
-                  initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.Variable(tf.constant(0.1,shape=[self.args.rnn_size]),name="bias_1")
-        prediction_l1 = tf.nn.relu(tf.nn.xw_plus_b(output_f,W,b,name="scores1"))
-        W1 = tf.get_variable(
-                  "W2",
-                  shape=[self.args.rnn_size, self.args.class_size],
-                  initializer=tf.contrib.layers.xavier_initializer())
-        b1 = tf.Variable(tf.constant(0.1,shape=[self.args.class_size]),name="bias_2")
+        if self.keep_prob == 1.0:
+          phase = True
+        else:
+          phase = False
+        prediction_l1 = tf.contrib.layers.fully_connected(output_f, self.args.rnn_size, 
+                                             activation_fn=None,
+                                             scope='layer1')
+       
+        prediction_l1 = tf.contrib.layers.batch_norm(prediction_l1, 
+                                          center=True, scale=True, 
+                                          is_training=phase,activation_fn=tf.nn.relu,
+                                          scope=scope)
         
-        self.prediction = tf.nn.softmax(tf.nn.xw_plus_b(prediction_l1,W1,b1,name="scores2"))
-        
+        prediction_l2 = tf.contrib.layers.fully_connected(prediction_l1, self.args.class_size, 
+                                             activation_fn=None,
+                                             scope='layer2')
+        self.prediction = tf.nn.softmax(prediction_l2)
+
         print self.prediction
         
         
